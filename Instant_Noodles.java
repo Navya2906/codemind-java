841a2875d5abc21d48251dfc691645ec0fcd29ec
Navya <155944279+Navya2906@users.noreply.github.com>
2024-01-13 14:18:11 +0530

    import java.util.*; public class Noodles {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int z=x*y;         System.out.printf("%d",z);     } }

---
+++ Instant_Noodles.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Noodles
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=x*y;
+        System.out.printf("%d",z);
+    }
+}

6a60708fda4f2f9a0d8e6a8d2262baaba71db6ff
Navya <155944279+Navya2906@users.noreply.github.com>
2024-01-15 10:00:54 +0530

    import java.util.*; public class Kmph {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         double a=sc.nextDouble();         double b=a*0.277778;         System.out.printf("%.2f",b);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Kmph
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextDouble();
+        double b=a*0.277778;
+        System.out.printf("%.2f",b);
+    }
+}

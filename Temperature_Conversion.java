d245fbe6065ae4e53b2bf5fdb4304a7466022140
Navya <155944279+Navya2906@users.noreply.github.com>
2024-01-17 15:50:37 +0530

    import java.util.*; public class temp {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         double a=sc.nextDouble();         double b=32+(a*9/5);         System.out.printf("%.2f ",b);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class temp
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        double a=sc.nextDouble();
+        double b=32+(a*9/5);
+        System.out.printf("%.2f
+",b);
+    }
+}

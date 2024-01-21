da95ed405701ba1c99bd4c3ba7a05e80939bb7d8
Navya <155944279+Navya2906@users.noreply.github.com>
2024-01-21 19:05:32 +0530

    import java.util.*; public class Height {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         {             int x=sc.nextInt();             int y=sc.nextInt();             int z=Math.max(x,y);             System.out.println(z);         }     } }

---
+++ Height.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Height
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        {
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int z=Math.max(x,y);
+            System.out.println(z);
+        }
+    }
+}

05ea105a595894e815e0a8bba6d2bbd434d87cb9
Navya <155944279+Navya2906@users.noreply.github.com>
2024-01-22 09:39:36 +0530

    import java.util.Scanner; public class InteriorDesign {     public static void main(String[] args)     {         Scanner scanner = new Scanner(System.in);         int x = scanner.nextInt();         if(x>98)         {             System.out.println("YES");         }         else         {             System.out.println("NO");         }     } }

---
+++ Fever.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class InteriorDesign 
+{
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        if(x>98)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}

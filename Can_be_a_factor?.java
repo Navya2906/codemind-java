6d5239799811051b4df3871194da902d225339e3
Navya <155944279+Navya2906@users.noreply.github.com>
2024-01-25 20:07:29 +0530

    import java.util.*; public class CanBeAFactor {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int t=sc.nextInt();         while(t-->0)         {             int a=sc.nextInt();             int b=sc.nextInt();             int c=Math.abs(a-b);             if(c>=a||a==b)             {                 System.out.println("YES");             }             else             {                 System.out.println("NO");             }         }     } }

---
+++ Can_be_a_factor?.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class CanBeAFactor
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=Math.abs(a-b);
+            if(c>=a||a==b)
+            {
+                System.out.println("YES");
+            }
+            else
+            {
+                System.out.println("NO");
+            }
+        }
+    }
+}

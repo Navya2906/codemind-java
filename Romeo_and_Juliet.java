857b2866c6a365f420ef2d28aee7d778e7e57e38
Navya <155944279+Navya2906@users.noreply.github.com>
2024-01-25 20:29:57 +0530

    import java.util.*; public class RomeoAndJuliet {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int z=sc.nextInt();         int a=5*x+10*y;         int b=a/z;         System.out.println(b);     } }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class RomeoAndJuliet
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        int a=5*x+10*y;
+        int b=a/z;
+        System.out.println(b);
+    }
+}

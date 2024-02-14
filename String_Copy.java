8c076e434e21392427d844106aceb4c65ed3bbdc
Navya <155944279+Navya2906@users.noreply.github.com>
2024-02-14 10:17:22 +0530

    import java.util.*; public class StringCopy {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         System.out.println(s);     } }

---
+++ String_Copy.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class StringCopy
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.println(s);
+    }
+}
